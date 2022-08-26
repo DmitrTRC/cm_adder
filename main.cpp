bc8535f31df55bef14bd70b321a4d0ae4dfe8c7f
Dev Spain <9984398@gmail.com>
2022-08-26 17:23:35 +0300

    Tested adding initials structure

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <filesystem>
 
 
 int main (int argc, char **argv) {
-    std::cout << "CMake create project." << std::endl;
+    if (argc != 2) {
+        std::cout << "Usage: " << argv[0] << " <project-name>" << std::endl;
+        return 1;
+    }
+
+    namespace fs = std::filesystem;
+
+    fs::path projectPath = argv[1];
+    fs::create_directory (projectPath);
+    fs::create_directory (projectPath / "src");
+    fs::create_directory (projectPath / "bin");
+    fs::create_directory (projectPath / "tests");
+    fs::create_directory (projectPath / "docs");
     return 0;
 }
